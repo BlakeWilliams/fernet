dee809fbf1f50f11220e88f583f080fb7a32ffba
Blake Williams <blake@blakewilliams.me>
2023-11-11 20:46:09 -0500

    Better naming, replace Use with Before

--- subrouter.go
+++ subrouter.go
@@ -18,28 +18,25 @@ type (
 	// SubRouter is similar to fernet.Router, but accepts a type that implements
 	// the FromRequest interface that will be initialized each request and passed
 	// to the handler as the third argument.
-	SubRouter[T RequestContext, Child FromRequest[T]] struct {
-		parent     Registerable[T]
-		root       *SubRouterGroup[T, Child]
-		middleware []func(context.Context, T, Handler[T])
+	SubRouter[T RequestContext, RequestData FromRequest[T]] struct {
+		parent Registerable[T]
+		root   *SubRouterGroup[T, RequestData]
 	}
 
 	// SubRouterHandler is the signature for SubRouter handlers. It accepts the
 	// standard context.Context, and T RequestContext, but also a third argument
 	// that implements the FromRequest interface.
-	SubRouterHandler[T RequestContext, Child FromRequest[T]] func(context.Context, T, Child)
+	SubRouterHandler[T RequestContext, RequestData FromRequest[T]] func(context.Context, T, RequestData)
 
 	// SubRouterRoutable ensures consistency across all SubRouter based types.
-	SubRouterRoutable[T RequestContext, Child FromRequest[T]] interface {
-		Match(string, string, SubRouterHandler[T, Child])
-		Get(string, SubRouterHandler[T, Child])
-		Post(string, SubRouterHandler[T, Child])
-		Put(string, SubRouterHandler[T, Child])
-		Patch(string, SubRouterHandler[T, Child])
-		Delete(string, SubRouterHandler[T, Child])
-
-		Use(func(context.Context, T, Handler[T]))
-		Group(string) *SubRouterGroup[T, Child]
+	SubRouterRoutable[T RequestContext, RequestData FromRequest[T]] interface {
+		Match(string, string, SubRouterHandler[T, RequestData])
+		Get(string, SubRouterHandler[T, RequestData])
+		Post(string, SubRouterHandler[T, RequestData])
+		Put(string, SubRouterHandler[T, RequestData])
+		Patch(string, SubRouterHandler[T, RequestData])
+		Delete(string, SubRouterHandler[T, RequestData])
+		Before(func(context.Context, T, RequestData) bool)
 	}
 
 	placeholderFromRequest struct{}
@@ -54,61 +51,60 @@ var _ SubRouterRoutable[*RootRequestContext, *placeholderFromRequest] = &SubRout
 // that accept a type that implements the FromRequest interface. Each request
 // will initialize a new instance of the type, call `FromRequest` on it, and
 // pass it to the handler if the method returns true.
-func NewSubRouter[Parent RequestContext, Child FromRequest[Parent]](r Registerable[Parent], dataType Child) *SubRouter[Parent, Child] {
-	return &SubRouter[Parent, Child]{
+func NewSubRouter[Parent RequestContext, RequestData FromRequest[Parent]](r Registerable[Parent], dataType RequestData) *SubRouter[Parent, RequestData] {
+	return &SubRouter[Parent, RequestData]{
 		parent: r,
-		root: &SubRouterGroup[Parent, Child]{
-			prefix:     "",
-			parent:     r,
-			middleware: make([]func(context.Context, Parent, Handler[Parent]), 0),
+		root: &SubRouterGroup[Parent, RequestData]{
+			prefix:  "",
+			parent:  r,
+			befores: make([]func(context.Context, Parent, RequestData) bool, 0),
 		},
-		middleware: make([]func(context.Context, Parent, Handler[Parent]), 0),
 	}
 }
 
 // RawMatch implements the Registerable interface and forwards the call to the
 // parent router. This allows subrouters and groups to be registered with the
 // subrouter.
-func (r *SubRouter[T, Child]) RawMatch(method string, path string, fn Handler[T]) {
+func (r *SubRouter[T, RequestData]) RawMatch(method string, path string, fn Handler[T]) {
 	r.parent.RawMatch(method, path, fn)
 }
 
 // Match registers the given handler with the given method and path.
-func (r *SubRouter[T, Child]) Match(method string, path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouter[T, RequestData]) Match(method string, path string, fn SubRouterHandler[T, RequestData]) {
 	r.root.Match(method, path, fn)
 }
 
 // Get registers a GET handler with the given path.
-func (r *SubRouter[T, Child]) Get(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouter[T, RequestData]) Get(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodGet, path, fn)
 }
 
 // Post registers a POST handler with the given path.
-func (r *SubRouter[T, Child]) Post(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouter[T, RequestData]) Post(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodPost, path, fn)
 }
 
 // Put registers a PUT handler with the given path.
-func (r *SubRouter[T, Child]) Put(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouter[T, RequestData]) Put(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodPut, path, fn)
 }
 
 // Patch registers a PATCH handler with the given path.
-func (r *SubRouter[T, Child]) Patch(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouter[T, RequestData]) Patch(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodPatch, path, fn)
 }
 
 // Delete registers a DELETE handler with the given path.
-func (r *SubRouter[T, Child]) Delete(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouter[T, RequestData]) Delete(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodDelete, path, fn)
 }
 
 // Use registers a middleware function that will be called before each handler.
-func (r *SubRouter[T, Child]) Use(fn func(context.Context, T, Handler[T])) {
-	r.middleware = append(r.middleware, fn)
+func (r *SubRouter[T, RequestData]) Before(fn func(context.Context, T, RequestData) bool) {
+	r.root.Before(fn)
 }
 
 // Group returns a new SubRouterGroup with the given prefix.
-func (r *SubRouter[T, Child]) Group(prefix string) *SubRouterGroup[T, Child] {
+func (r *SubRouter[T, RequestData]) Group(prefix string) *SubRouterGroup[T, RequestData] {
 	return r.root.Group(prefix)
 }
--- subrouter_group.go
+++ subrouter_group.go
@@ -7,11 +7,10 @@ import (
 )
 
 // SubRouterGroup is a group of routes from a SubRouter that share a common
-// prefix and maintain their own middleware stack.
-type SubRouterGroup[T RequestContext, Child FromRequest[T]] struct {
-	prefix     string
-	parent     Registerable[T]
-	middleware []func(context.Context, T, Handler[T])
+type SubRouterGroup[T RequestContext, RequestData FromRequest[T]] struct {
+	prefix  string
+	parent  Registerable[T]
+	befores []func(context.Context, T, RequestData) bool
 }
 
 var _ SubRouterRoutable[*RootRequestContext, *placeholderFromRequest] = &SubRouter[*RootRequestContext, *placeholderFromRequest]{}
@@ -19,75 +18,75 @@ var _ SubRouterRoutable[*RootRequestContext, *placeholderFromRequest] = &SubRout
 // RawMatch implements the Registerable interface and forwards the call to the
 // parent router. This allows subrouters and groups to be registered with the
 // subrouter.
-func (r *SubRouterGroup[T, Child]) RawMatch(method string, path string, fn Handler[T]) {
+func (r *SubRouterGroup[T, RequestData]) RawMatch(method string, path string, fn Handler[T]) {
 	r.parent.RawMatch(method, path, fn)
 }
 
 // Match registers the given handler with the given method and path.
-func (r *SubRouterGroup[T, Child]) Match(method string, path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouterGroup[T, RequestData]) Match(method string, path string, fn SubRouterHandler[T, RequestData]) {
 	r.parent.RawMatch(method, path, r.wrap(fn))
 }
 
 // Get registers a GET handler with the given path.
-func (r *SubRouterGroup[T, Child]) Get(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouterGroup[T, RequestData]) Get(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodGet, path, fn)
 }
 
 // Post registers a POST handler with the given path.
-func (r *SubRouterGroup[T, Child]) Post(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouterGroup[T, RequestData]) Post(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodPost, path, fn)
 }
 
 // Put registers a PUT handler with the given path.
-func (r *SubRouterGroup[T, Child]) Put(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouterGroup[T, RequestData]) Put(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodPut, path, fn)
 }
 
 // Patch registers a PATCH handler with the given path.
-func (r *SubRouterGroup[T, Child]) Patch(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouterGroup[T, RequestData]) Patch(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodPatch, path, fn)
 }
 
 // Delete registers a DELETE handler with the given path.
-func (r *SubRouterGroup[T, Child]) Delete(path string, fn SubRouterHandler[T, Child]) {
+func (r *SubRouterGroup[T, RequestData]) Delete(path string, fn SubRouterHandler[T, RequestData]) {
 	r.Match(http.MethodDelete, path, fn)
 }
 
-// Use registers a middleware function that will be called before each handler.
-func (r *SubRouterGroup[T, Child]) Use(fn func(context.Context, T, Handler[T])) {
-	r.middleware = append(r.middleware, fn)
+// Use registers a befores function that will be called before each handler.
+func (r *SubRouterGroup[T, RequestData]) Before(fn func(context.Context, T, RequestData) bool) {
+	r.befores = append(r.befores, fn)
 }
 
 // Group returns a new SubRouterGroup with the given prefix.
-func (r *SubRouterGroup[T, Child]) Group(prefix string) *SubRouterGroup[T, Child] {
-	return &SubRouterGroup[T, Child]{
-		prefix:     r.prefix + prefix,
-		parent:     r,
-		middleware: make([]func(context.Context, T, Handler[T]), 0),
+func (r *SubRouterGroup[T, RequestData]) Group(prefix string) *SubRouterGroup[T, RequestData] {
+	return &SubRouterGroup[T, RequestData]{
+		prefix:  r.prefix + prefix,
+		parent:  r,
+		befores: make([]func(context.Context, T, RequestData) bool, 0),
 	}
 }
 
-func (r *SubRouterGroup[T, Child]) wrap(fn SubRouterHandler[T, Child]) Handler[T] {
-	var t Child
-	childType := reflect.TypeOf(t)
-	isPointer := childType.Kind() == reflect.Ptr
+func (r *SubRouterGroup[T, RequestData]) wrap(fn SubRouterHandler[T, RequestData]) Handler[T] {
+	var t RequestData
+	requestDataType := reflect.TypeOf(t)
+	isPointer := requestDataType.Kind() == reflect.Ptr
 
 	if isPointer {
-		childType = childType.Elem()
+		requestDataType = requestDataType.Elem()
 	}
 
 	return func(ctx context.Context, rc T) {
-		newChild := reflect.New(childType)
+		newRequestData := reflect.New(requestDataType)
 		if !isPointer {
-			newChild = newChild.Elem()
+			newRequestData = newRequestData.Elem()
 		}
-		child := newChild.Interface()
-		success := (child).(FromRequest[T]).FromRequest(ctx, rc)
+		requestData := newRequestData.Interface()
+		success := (requestData).(FromRequest[T]).FromRequest(ctx, rc)
 
 		if !success {
 			return
 		}
 
-		fn(ctx, rc, child.(Child))
+		fn(ctx, rc, requestData.(RequestData))
 	}
 }
